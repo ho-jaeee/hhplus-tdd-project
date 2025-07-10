a2da042be5dc6319a3f0cdf05a65dd6b6d2c6020
unknown <wogh0592@gmail.com>
2025-07-10 16:54:43 +0900

    Step1 : 통합테스트 코드 작성

--- src/test/java/io/hhplus/tdd/point/integration/PointIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/integration/PointIntegrationTest.java
@@ -17,6 +17,7 @@ import io.hhplus.tdd.point.PointService;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @SpringBootTest// Mockito가 아닌 스프링을 올려서 하는 테스트
 public class PointIntegrationTest {
@@ -36,7 +37,7 @@ public class PointIntegrationTest {
         pointHistoryTable = new PointHistoryTable(); // 직접 생성
         pointService = new PointService(pointHistoryTable, userPointTable); // 수동 주입
 
-        ((UserPointTable) userPointTable).insertOrUpdate(1L,10000L);
+        ((UserPointTable) userPointTable).insertOrUpdate(1L,10000L);//현재 포인트 초기화
     }
 
 
@@ -46,7 +47,7 @@ public class PointIntegrationTest {
 
         //given
         long userId = 1L;
-        long existingPoint = 10000L;
+        long existingPoint = 10000L;//현재 포인트 10,000원으로 초기화 ↑
 
         // when
         //pointService.pointCharge(userId, existingPoint);
@@ -55,24 +56,25 @@ public class PointIntegrationTest {
         // then
         // 결과 검증
         assertThat(result).isEqualTo(existingPoint);//Stub
-
-
     }
 
     @Test
     //포인트 충전 /point/{id}/charge
     void 포인트_충전_증가(){
+
         // given
         long userId = 1L;
         long chargeAmount = 3000L;
+
+        //expected
         long existingPoint = 13000L;
 
-
         // when
-        UserPoint charge = pointService.pointCharge(userId, chargeAmount);
+        UserPoint charge = pointService.pointCharge(userId, chargeAmount);//서비스 호출
 
-        // then
-        assertThat(charge.point()).isEqualTo(existingPoint);//Stub 10,000 + 3,000
+        // then(point + chargeAmount)
+        //결과검증
+        assertThat(charge.point()).isEqualTo(existingPoint);//Stub 현재포인트 10,000 + 충전포인트 3,000 =13,000원
 
     }
 
@@ -83,13 +85,15 @@ public class PointIntegrationTest {
         //given
         long userId = 1L;
         long usingPoint = 2000L;
+
+        //expected
         long existingPoint = 8000L;
 
         // when
-        UserPoint userPoint = pointService.pointUse(userId, usingPoint);
+        UserPoint userPoint = pointService.pointUse(userId, usingPoint);//서비스 호출
 
         // then(point - usingPoint)
-        assertThat(userPoint.point()).isEqualTo(existingPoint);//Stub 10,000 - 2,000
+        assertThat(userPoint.point()).isEqualTo(existingPoint);//Stub 현재포인트 10,000 - 충전포인트 2,000 = 8,000원
 
     }
 
@@ -102,28 +106,76 @@ public class PointIntegrationTest {
         long chargePoint = 5000L;
         long usingPoint = 2000L;
         long now = System.currentTimeMillis();
-
-
-        // when
-        // UserPoint chargedPoint = pointService.pointCharge(userId, chargePoint);
-        // UserPoint userPoint = pointService.pointUse(userId, usingPoint);
-
         ((PointHistoryTable) pointHistoryTable).insert(userId, chargePoint, TransactionType.CHARGE, now);
         ((PointHistoryTable) pointHistoryTable).insert(userId, usingPoint, TransactionType.USE, now + 1);
 
+        // when
         List<PointHistory> pointHistories = pointService.getPointHistories(userId);
-
 
         // then
         assertThat(pointHistories).hasSize(2);
-
         assertThat(pointHistories.get(0).type()).isEqualTo(TransactionType.CHARGE);
         assertThat(pointHistories.get(0).amount()).isEqualTo(5000L);
         assertThat(pointHistories.get(1).type()).isEqualTo(TransactionType.USE);
         assertThat(pointHistories.get(1).amount()).isEqualTo(2000L);
 
+    }
+
+    //잔고가 부족 할 경우, 포인트 사용은 실패
+    @Test
+    void 포인트_사용_실패_잔고부족() {
+
+        // given
+        long userId = 1L;
+        long usePoint = 100000L;  // 현재포인트 10,000원, 사용포인트 100,000원
+
+        // when & then
+        assertThatThrownBy(() -> pointService.pointUse(userId, usePoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("포인트 부족");
+    }
+
+    //포인트 사용, 충전 시 0원 사용은 안된다.(이건 근데 서비스 보다는 controller에서 입력값을 아예 안받는게 더 좋은거 같음(서비스의 분기문이 너무 많아짐)
+    @Test
+    void 포인트_0원_충전_불가(){
+
+        //given
+        long userId = 1L;
+        long chargeAmount = 0L;
+
+        //when/then
+        assertThatThrownBy(()-> pointService.pointCharge(userId, chargeAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("충전 금액은 0원보다 커야 합니다.");
+    }
+
+    @Test
+    void 포인트_0원_사용_불가(){
+
+        //given
+        long userId = 1L;
+        long useAmount = 0L;
+
+        //when/then
+        assertThatThrownBy(()-> pointService.pointUse(userId, useAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("사용 금액은 0원보다 커야 합니다.");
+    }
 
 
+    //포인트 충전은 1,000원 단위로만 가능
+    //셀프 주차장 생각을 함, 토스페이, 카카오페이, 네이버페이, 쿠팡페이등 여러가지 페이는 1원단위까지 충전가능함
+    @Test
+    void 포인트_충전_단위_1000(){
+
+        //given
+        long userId = 1L;
+        long chargeAmount = 15220L;
+
+        //when/then
+        assertThatThrownBy(()-> pointService.pointCharge(userId, chargeAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("충전금액은 1,000원 단위로 충전해주시기 바랍니다.");
     }
 
 
@@ -134,6 +186,5 @@ public class PointIntegrationTest {
         userPointTable = null;
         pointHistoryTable = null;
     }
-
 
 }
